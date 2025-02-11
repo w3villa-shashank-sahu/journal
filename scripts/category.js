370e437e1292d7841ab35e80975d2869ebdea491
w3villa-shashank-sahu <shashank.sahu@w3villa.com>
2025-02-11 18:34:19 +0530

    completed responsiveness

--- scripts/category.js
+++ scripts/category.js
@@ -1,4 +1,4 @@
-function initializeCarousel(className, slidesCount) {
+function initializeCarousel(className, slidesCount, breakpoints) {
     $(className).slick({
         dots: true,
         infinite: false,
@@ -6,24 +6,40 @@ function initializeCarousel(className, slidesCount) {
         slidesToShow: slidesCount,
         slidesToScroll: slidesCount,
         arrows: false,
-        responsive: Array.from({ length: slidesCount - 1 }, (_, index) => {
-            const remainingSlides = slidesCount - index - 1;
-            return {
-                breakpoint: 1400 - (index * 300),
-                settings: {
-                    slidesToShow: remainingSlides,
-                    slidesToScroll: remainingSlides,
-                    infinite: true,
-                    dots: true
-                }
-            };
-        })
+        responsive: breakpoints.map(bp => ({
+            breakpoint: bp.width,
+            settings: {
+                slidesToShow: Math.min(bp.slidesToShow, slidesCount),
+                slidesToScroll: Math.min(bp.slidesToShow, slidesCount),
+                infinite: true,
+                dots: true
+            }
+        }))
     });
 }
 
 $(document).ready(function () {
-    initializeCarousel('.carousel3', 3);
-    initializeCarousel('.carousel5', 5);
-    initializeCarousel('.carousel4', 4);
-    initializeCarousel('.carousel9', 9);
+    const breakpoints = [
+        { width: 1400, slidesToShow: 4 },
+        { width: 1200, slidesToShow: 3 },
+        { width: 992, slidesToShow: 2 },
+        { width: 500, slidesToShow: 1 }
+    ];
+
+    const breakpoints5 = [
+        { width: 1400, slidesToShow: 4 },
+        { width: 900, slidesToShow: 3 },
+        { width: 700, slidesToShow: 2 },
+    ];
+    const breakpoints9 = [
+
+        { width: 1000, slidesToShow: 7 },
+        { width: 750, slidesToShow: 5 },
+        { width: 450, slidesToShow: 3}
+    ];
+
+    initializeCarousel('.carousel3', 3, breakpoints);
+    initializeCarousel('.carousel5', 5, breakpoints5);
+    initializeCarousel('.carousel4', 4, breakpoints);
+    initializeCarousel('.carousel9', 9, breakpoints9);
 });
